a3591ae7d27ced79f6f9eb7118ed0b5c85e49b7a
Guo Zhang <zhangguo@quanttide.com>
2026-03-15 21:51:06 +0800

    添加默认活动启动脚本

--- src/thera/__init__.py
+++ src/thera/__init__.py
@@ -1,10 +1,6 @@
 from thera.__main__ import Thera, main
-from thera.meta import get_system_info, get_system_state, SystemState
 
 __all__ = [
     "Thera",
     "main",
-    "get_system_info",
-    "get_system_state",
-    "SystemState",
 ]
